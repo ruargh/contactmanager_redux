30c3e0ce62b0e9a92c1cde1b8dee50b331d556bd
Ruargh <ruargh@gmail.com>
2018-11-02 18:26:04 +0200

    added commented section to store.js for devtools breaking in firefox - @Todo: Test a build with this

--- src/store.js
+++ src/store.js
@@ -12,6 +12,12 @@ const store = createStore(
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    /* maybe try for Firefox
+    
+     typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+
+     https://github.com/prescottprue/react-redux-firebase/blob/master/examples/complete/simple/src/store.js
+     */
   )
 );
 
